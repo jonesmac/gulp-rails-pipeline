e39c408afcada2a32402a12c98556424a423dd11
Matt Jones <matt.jones@dxydoes.com>
2015-08-10 11:02:17 -0400

    use scss instead of sass

--- gulp/config.js
+++ gulp/config.js
@@ -11,28 +11,12 @@ module.exports = {
     src: sourceFiles + "/stylesheets/**/*.{sass,scss}",
     dest: publicAssets + "/stylesheets",
     settings: {
-      indentedSyntax: true, // Enable .sass syntax!
-      imagePath: '/assets/images' // Used by the image-url helper
+      imagePath: '/assets/images', // Used by the image-url helper
     }
   },
   images: {
     src: sourceFiles + "/images/**",
     dest: publicAssets + "/images"
-  },
-  iconFont: {
-    name: 'Gulp Rails Icons',
-    src: sourceFiles + "/icons/*.svg",
-    dest: publicAssets + '/fonts',
-    sassDest: sourceFiles + '/stylesheets/base',
-    template: './gulp/tasks/iconFont/template.sass',
-    sassOutputName: '_iconFont.sass',
-    fontPath: '/assets/fonts',
-    className: 'icon',
-    options: {
-      fontName: 'gulp-rails-icons',
-      appendCodepoints: true,
-      normalize: false
-    }
   },
   browserify: {
     bundleConfigs: [{
